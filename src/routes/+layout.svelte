2e48dae6c321440754f46771f7bc2cc944903535
dingyuchen <dingyuchen@gmail.com>
2023-07-21 23:05:05 +0800

    Add transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,17 @@
 	import '../app.css';
 	import Footer from './footer.svelte';
 	import Header from './header.svelte';
+	import PageTransition from './transition.svelte';
+	
+	export let data
 </script>
 
 <div class="flex flex-col items-center">
 	<Header />
 	<main class="max-w-prose w-full flex flex-col m-4 mb-10 items-center">
-		<slot />
+		<PageTransition url={data.url}>
+			<slot />
+		</PageTransition>
 	</main>
 	<Footer />
 </div>
---
+++ src/routes/transition.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { fade } from "svelte/transition"
+    
+    export let url:string
+</script>
+
+{#key url}
+    <div class="h-full w-full" in:fade>
+        <slot/>
+    </div>
+{/key}
